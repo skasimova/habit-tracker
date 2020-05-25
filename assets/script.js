0a2d7046a9167f84d2829b3dc7e8c44d48f7a423
Alex K <contact.alexsophia@gmail.com>
2020-05-25 15:19:48 +0300

    Worked on the script.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,58 @@
-let form= document.getElementById('add-habit');
+let form = document.getElementById('add-habit');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
 
-    alert('hi');
+    createHabit('test');
 })
 
-let repetitions = document.getElementById('repetitions');
 
-let addButton = document.getElementById('add-button');
+function createHabit(habitText) {
+    let habitContainer = document.getElementById('habit-container');
+
+    let habit = document.createElement('div');
+    habit.setAttribute('class', 'habit');
+
+    let habitHeader = document.createElement('div');
+    habitHeader.setAttribute('class', 'habit-header');
+    habitHeader.innerHTML = 'Added habit';
+
+    let habitName = document.createElement('div');
+    habitName.setAttribute('class', 'habit-name');
+
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', 'btn btn-sm btn-outline-danger');
+    deleteButton.innerHTML = 'x';
+
+    habitHeader.appendChild(habitName);
+    habitHeader.appendChild(deleteButton);
+
+    let progress = document.createElement('div');
+    progress.setAttribute('class', 'progress');
+
+    let progressBar = document.createElement('div');
+    progressBar.setAttribute('class', 'progress-bar');
+    progressBar.setAttribute('style', 'width: ' + '' + '%');
+    progressBar.innerHTML = '' + '%';
+
+    progress.appendChild(progressBar);
+
+    let loggedHabits = document.createElement('div');
+    loggedHabits.setAttribute('class', 'logged-habits');
+
+    let numOfRepetitions = document.createElement('div');
+    numOfRepetitions.setAttribute('class', 'num-of-times');
+
+    let plusButton = document.createElement('button');
+    plusButton.setAttribute('class', 'btn btn-success');
+    plusButton.innerHTML = '+';
+
+    loggedHabits.appendChild(numOfRepetitions);
+    loggedHabits.appendChild(plusButton);
+
+    habit.appendChild(habitHeader);
+    habit.appendChild(progress);
+    habit.appendChild(loggedHabits);
+
+    habitContainer.appendChild(habit);
+}
