184b1e2594900a3dfe46bb51390cc9ccc6876561
Alex K <contact.alexsophia@gmail.com>
2020-05-25 14:11:26 +0300

    Improved the structure of the page.

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+let form= document.getElementById('add-habit');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    alert('hi');
+})
+
+let repetitions = document.getElementById('repetitions');
+
+let addButton = document.getElementById('add-button');
